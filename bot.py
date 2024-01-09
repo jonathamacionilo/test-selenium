6df5b4124455ebd36d88c2fbfcba516eda4e2b7d
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 20:46:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,18 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-class Bot:
+class BotGLPI:
     def __init__(self):
         self.driver = None
 
     def initiate_bot(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        # Especifique o caminho local do chromedriver na raiz do seu projeto
+        chromedriver_path = "./chromedriver"
+        
+        # Inicialize o WebDriver do Chrome utilizando o chromedriver local
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
     
-    def acessar_youtube(self):
+    def abrir_pagina_youtube(self):
         if self.driver is None:
             self.initiate_bot()
-        self.driver.get('https://youtube.com')
+        self.driver.get('https://www.youtube.com.br')
