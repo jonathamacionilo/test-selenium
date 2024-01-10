4a8862a930269b77b037bd8af4fe67a881a4b7ba
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 21:06:58 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,15 @@ class Bot:
     def initiate_bot(self):
         # Especifique o caminho local do chromedriver na raiz do seu projeto
         chromedriver_path = "./chromedriver"
+        # Opções do ChromeDriver
+        options = webdriver.ChromeOptions()
         
-        # Inicialize o WebDriver do Chrome utilizando o chromedriver local
-        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
+        # Desabilitar o sandbox para a execução na Streamlit Sharing
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        
+        # Inicialize o WebDriver do Chrome utilizando o chromedriver local e as opções definidas
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
     
     def abrir_pagina_youtube(self):
         if self.driver is None:
