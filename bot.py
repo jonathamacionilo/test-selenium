d1a541a23f163ef01dd4e3292b164ed05d0276b8
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 22:39:50 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,11 +12,16 @@ class Bot:
             # Enforce headless mode for Streamlit Cloud
             options = Options()
             options.headless = True
-
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-features=NetworkService")
+            options.add_argument("--window-size=1920x1080")
+            options.add_argument("--disable-features=VizDisplayCompositor")
             # Use GeckoDriverManager for driver management
-            self.driver = webdriver.Firefox(
-                service=Service(GeckoDriverManager().install()),
-                options=options
+            self.driver = webdriver.Chrome(
+                options=get_webdriver_options(), service=get_webdriver_service()
             )
         except Exception as e:
             print(f"Erro ao iniciar o bot: {e}\nVerifique a compatibilidade do driver e as configurações do Streamlit Cloud.")
