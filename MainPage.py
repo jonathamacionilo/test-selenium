1bd112cc67c3ac63738384f037b768fb01cfb2e9
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 23:00:10 -0300

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -1,22 +1,16 @@
 import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
 
 def verificar_acesso_youtube():
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-features=NetworkService")
-    options.add_argument("--window-size=1920x1080")
-    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.headless = True
 
     driver = None
     try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
         driver.get('https://www.youtube.com')
         return True
     except Exception as e:
