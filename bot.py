858b0d9157567ced43b4e5f5f6f00dae3d5a68e0
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 21:12:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 class Bot:
     def __init__(self):
@@ -8,15 +8,13 @@ class Bot:
 
     def initiate_bot(self):
         try:
-            # Configuração do serviço do ChromeDriver
-            chrome_service = Service(ChromeDriverManager().install())
-            
-            # Inicialização do WebDriver do Chrome utilizando o serviço do ChromeDriver
-            self.driver = webdriver.Chrome(service=chrome_service)
+            # Inicializa o WebDriver do Firefox usando o GeckoDriverManager
+            self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         except Exception as e:
             print(f"Erro ao iniciar o bot: {e}")
     
     def abrir_pagina_youtube(self):
         if self.driver is None:
             self.initiate_bot()
-        self.driver.get('https://www.youtube.com.br')
+        if self.driver:
+            self.driver.get('https://www.youtube.com.br')
