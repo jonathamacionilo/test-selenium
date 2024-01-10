4715838d3fdb28588efd0fb06bc8c6c124aa10ab
Jonatha Macionilo <67168807+jonathamacionilo@users.noreply.github.com>
2024-01-09 23:16:42 -0300

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -1,32 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
+from streamlit_selenium import st_selenium
 
 def abrir_youtube():
     try:
-        options = Options()
-        options.add_argument("--headless")
-        
-        # Defina o caminho do binário do Firefox
-        firefox_binary = '/path/to/your/firefox-binary'  # Substitua pelo caminho do seu binário do Firefox
-
-        # Inicializa o serviço do GeckoDriver usando o GeckoDriverManager
-        service = Service(GeckoDriverManager().install())
-
-        # Define o binário do Firefox para o WebDriver
-        options.binary_location = firefox_binary
-
-        # Inicializa o WebDriver do Firefox com as opções definidas
-        driver = webdriver.Firefox(service=service, options=options)
-        
-        driver.get('https://www.youtube.com/')
-        
-        st.write("YouTube acessado com sucesso!")
-        
-        driver.quit()
-        
+        # Inicia a sessão do navegador
+        with st_selenium() as browser:
+            # Abre o site do YouTube
+            browser.get("https://www.youtube.com/")
+            st.write("YouTube acessado com sucesso!")
+            
     except Exception as e:
         st.write(f"Não foi possível acessar o YouTube: {e}")
 
